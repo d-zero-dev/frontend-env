be4fdfdbe6d3da29616bbaeacb048a1366e7bc28
yusukehirao <yusukehirao@me.com>
2024-05-16 00:47:19 +0900

    refactor(builder): output files are sorted

--- packages/@d-zero/builder/fn/get-html-files.mjs
+++ packages/@d-zero/builder/fn/get-html-files.mjs
@@ -1,3 +1,5 @@
+import { pathComparator } from '@d-zero/shared/sort/path';
+
 /**
  * @typedef {Object} EleventyResultFile
  * @property {string} inputPath
@@ -11,22 +13,23 @@
  * @returns {EleventyResultFile[]}
  */
 export function getHtmlFiles(results) {
-	return results.flatMap((result) =>
-		result
-			.map((file) => {
-				if ('outputPath' in file && file.outputPath.endsWith('.html')) {
-					return file;
-				}
-			})
-			.filter(Boolean)
-			.toSorted(
-				/**
-				 *
-				 * @param {EleventyResultFile} a
-				 * @param {EleventyResultFile} b
-				 * @returns
-				 */
-				(a, b) => a.inputPath - b.inputPath,
-			),
-	);
+	return results
+		.flatMap((result) =>
+			result
+				.map((file) => {
+					if ('outputPath' in file && file.outputPath.endsWith('.html')) {
+						return file;
+					}
+				})
+				.filter(Boolean),
+		)
+		.toSorted(
+			/**
+			 *
+			 * @param {EleventyResultFile} a
+			 * @param {EleventyResultFile} b
+			 * @returns
+			 */
+			(a, b) => pathComparator(a.inputPath, b.inputPath),
+		);
 }
