40a46e0f67deb8f5d11f6ead0eccda0f4afd53a9
arktds <ariki.tds@gmail.com>
2024-06-14 11:32:10 +0900

    feat(scaffold): typed-query-selectorの追加

--- packages/@d-zero/scaffold/__assets/_libs/script/index.ts
+++ packages/@d-zero/scaffold/__assets/_libs/script/index.ts
@@ -1,3 +1,6 @@
+// querySelector() または querySelectorAll() を実行する場合、次の import 文を有効にしてください。
+// import 'typed-query-selector/strict.js';
+
 export function helloWorld() {
 	console.log('Hello, world!'); // eslint-disable-line no-console
 }
