8292cc90098076ad613c33162610e8536bd89ff7
YusukeHirao <hirao@d-zero.co.jp>
2024-07-05 12:49:43 +0900

    fix(scaffold): fix to ignore `htdocs` on lint staged process

--- packages/@d-zero/scaffold/lint-staged.config.mjs
+++ packages/@d-zero/scaffold/lint-staged.config.mjs
@@ -1,3 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import lintStagedConfigGenerator from '@d-zero/lint-staged-config';
-export default lintStagedConfigGenerator();
+export default lintStagedConfigGenerator({
+	ignore: ['**/htdocs/**/*'],
+});
