c62c5327bd7f142960950fbe4300905301d34a6f
YusukeHirao <hirao@d-zero.co.jp>
2024-07-05 16:53:34 +0900

    fix(scaffold): fix ignore path within lint-staged

--- packages/@d-zero/scaffold/lint-staged.config.mjs
+++ packages/@d-zero/scaffold/lint-staged.config.mjs
@@ -1,5 +1,7 @@
+import path from 'node:path';
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import lintStagedConfigGenerator from '@d-zero/lint-staged-config';
 export default lintStagedConfigGenerator({
-	ignore: ['**/htdocs/**/*'],
+	ignore: [path.resolve(process.cwd(), 'htdocs', '**', '*')],
 });
