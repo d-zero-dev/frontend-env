c09eebb1cd1c982f8fa82e2bfbb91677171ed49e
YusukeHirao <hirao@d-zero.co.jp>
2024-07-31 18:00:21 +0900

    chore(changelog): CHANGELOG.mdファイルをtextlintの対象から除外

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import lintStagedConfigGenerator from '@d-zero/lint-staged-config';
-export default lintStagedConfigGenerator();
+export default lintStagedConfigGenerator({
+	ignore: [
+		{
+			textlint: 'CHANGELOG.md',
+		},
+	],
+});
--- packages/@d-zero/scaffold/lint-staged.config.mjs
+++ packages/@d-zero/scaffold/lint-staged.config.mjs
@@ -3,5 +3,10 @@ import path from 'node:path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import lintStagedConfigGenerator from '@d-zero/lint-staged-config';
 export default lintStagedConfigGenerator({
-	ignore: [path.resolve(process.cwd(), 'htdocs', '**', '*')],
+	ignore: [
+		path.resolve(process.cwd(), 'htdocs', '**', '*'),
+		{
+			textlint: 'CHANGELOG.md',
+		},
+	],
 });
