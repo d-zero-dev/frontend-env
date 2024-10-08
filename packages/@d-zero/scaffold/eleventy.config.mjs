0c9deedf73de8547c1bb7d825c6ed5d2eebe61f8
YusukeHirao <hirao@d-zero.co.jp>
2024-10-08 15:29:04 +0900

    refactor(scaffold): update eleventy config

--- packages/@d-zero/scaffold/eleventy.config.mjs
+++ packages/@d-zero/scaffold/eleventy.config.mjs
@@ -3,26 +3,21 @@ import path from 'node:path';
 import eleventy from '@d-zero/builder/11ty';
 
 export default function (eleventyConfig) {
-	// eleventyConfig.addGlobalData('publicDir', '@static');
-	// eleventyConfig.addGlobalData('outputCssDir', 'css');
-	// eleventyConfig.addGlobalData('outputJsDir', 'js');
-	// eleventyConfig.addGlobalData('outputImgDir', 'img');
-
 	eleventyConfig.addGlobalData('alias', {
 		'@': path.resolve(import.meta.dirname, '__assets', '_libs'),
 	});
 
-	eleventyConfig.setPugOptions({
-		basedir: path.resolve(import.meta.dirname, '__assets', '_libs'),
-	});
-
-	if (process.env.NODE_ENV === 'production') {
-		eleventyConfig.addGlobalData('prettier', true);
-		// eleventyConfig.addGlobalData('minifier', { minifyJS: false });
-		// eleventyConfig.addGlobalData('lineBreak', '\r\n');
-		// eleventyConfig.addGlobalData('charset', 'shift_jis');
-		// eleventyConfig.addGlobalData('pathFormat', 'preserve');
-	}
+	// eleventyConfig.addGlobalData('outputCssDir', 'css');
+	// eleventyConfig.addGlobalData('outputJsDir', 'js');
+	// eleventyConfig.addGlobalData('outputImgDir', 'img');
+	// eleventyConfig.addGlobalData('prettier', false);
+	/**
+	 * @see https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference
+	 */
+	// eleventyConfig.addGlobalData('minifier', { minifyJS: false });
+	// eleventyConfig.addGlobalData('lineBreak', '\r\n');
+	// eleventyConfig.addGlobalData('charset', 'shift_jis');
+	// eleventyConfig.addGlobalData('pathFormat', 'directory');
 
 	return eleventy(eleventyConfig);
 }
