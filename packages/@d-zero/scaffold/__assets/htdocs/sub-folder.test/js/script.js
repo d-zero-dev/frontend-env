9a732b3082a80d4ebc749fa23db70b586522231a
YusukeHirao <hirao@d-zero.co.jp>
2024-10-08 15:27:55 +0900

    refactor(scaffold): update sample codes

--- packages/@d-zero/scaffold/__assets/htdocs/sub-folder.test/js/script.js
+++
@@ -1,9 +0,0 @@
-import createMainScript, { __smLte } from '../../__assets/_libs/script/index';
-
-// eslint-disable-next-line no-console
-console.log(__smLte);
-
-createMainScript(() => {
-	// eslint-disable-next-line no-console
-	console.log('このスクリプトは、サブフォルダのテスト用です。');
-});
