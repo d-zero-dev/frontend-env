3d2486b21a390a0336ae69ffe4caa55cf4cbd4db
YusukeHirao <hirao@d-zero.co.jp>
2024-10-08 13:51:43 +0900

    test(builder): fix result for updating reporter

--- packages/@d-zero/builder/cli.spec.mjs
+++ packages/@d-zero/builder/cli.spec.mjs
@@ -16,7 +16,7 @@ beforeAll(cleanUp);
 afterAll(cleanUp);
 
 describe('CLI', () => {
-	test('build (no params)', async ({ tmpDir, task }) => {
+	test('build (no params)', async () => {
 		const { stdout } = await execa('build', [], {
 			cwd: path.resolve(process.cwd(), 'packages', '@d-zero', 'scaffold'),
 			failed: true,
@@ -33,44 +33,35 @@ describe('CLI', () => {
 				// Remove version
 				.replaceAll(/v\d+\.\d+\.\d+(?:[a-z]+(?:\.[\da-z]+)?)?/gi, '{version}')
 				// Remove size
-				.replaceAll(/\s*\d+\.\d{2}\s*(?:kB|MB|GB)/g, ' {size}')
-				// Remove random number
-				.replaceAll(/::inline-script::\d{1,2}/g, '::inline-script::{number}')
+				.replaceAll(/\s*\d+\.\d+\s*(?:kB|MB|GB)/g, ' {size}')
 				.split('\n'),
 		).toStrictEqual([
-			'vite {version} building for production...',
-			'transforming...',
-			'✓ 15 modules transformed.',
-			'rendering chunks...',
-			'computing gzip size...',
-			'../htdocs/img/bg-arrow.svg {size} │ gzip: {size}',
-			'../htdocs/index.html {size} │ gzip: {size}',
-			'../htdocs/sub-folder.test/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/000_home/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/302_form_complete/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/100_sub/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/210_blog_index/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/200_blog_index/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/301_form_confirm/index.html {size} │ gzip: {size}',
-			'../htdocs/__tmpl/300_form_input/index.html {size} │ gzip: {size}',
-			'../htdocs/css/style.css {size} │ gzip: {size}',
-			'../htdocs/js/script.js {size} │ gzip: {size}',
-			'../htdocs/::inline-script::{number} {size} │ gzip: {size}',
-			'✓ built in {time}',
-			'[11ty] Copied 14 files / Wrote 10 files in {time} ({time} each, {version})',
-			'Convert:',
-			'From                         | To                            | alias   | minifier',
-			'index.pug                    | index.html                    | enabled | enabled ',
-			'__tmpl/index.pug             | __tmpl/index.html             | enabled | enabled ',
-			'__tmpl/000_home.pug          | __tmpl/000_home.html          | enabled | enabled ',
-			'__tmpl/100_sub.pug           | __tmpl/100_sub.html           | enabled | enabled ',
-			'__tmpl/200_blog_index.pug    | __tmpl/200_blog_index.html    | enabled | enabled ',
-			'__tmpl/210_blog_index.pug    | __tmpl/210_blog_index.html    | enabled | enabled ',
-			'__tmpl/300_form_input.pug    | __tmpl/300_form_input.html    | enabled | enabled ',
-			'__tmpl/301_form_confirm.pug  | __tmpl/301_form_confirm.html  | enabled | enabled ',
-			'__tmpl/302_form_complete.pug | __tmpl/302_form_complete.html | enabled | enabled ',
-			'sub-folder.test/index.pug    | sub-folder.test/index.html    | enabled | enabled ',
+			'htdocs/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/000_home/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/100_sub/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/200_blog_index/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/210_blog_index/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/300_form_input/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/301_form_confirm/index.html {size} | gzip: {size}',
+			'htdocs/__tmpl/302_form_complete/index.html {size} | gzip: {size}',
+			'htdocs/css/style.css {size} | gzip: {size}',
+			'htdocs/js/script.js {size} | gzip: {size}',
+			'htdocs/sub-folder.test/index.html {size} | gzip: {size}',
+			'htdocs/sub-folder.test/css/style.css {size} | gzip: {size}',
+			'htdocs/sub-folder.test/js/script.js {size} | gzip: {size}',
+			'[11ty] Wrote 14 files in {time} ({time} each, {version})',
+			'File Transfer  preserve :',
+			'index.pug                    -> index.html                          -> index.html                   ',
+			'__tmpl/index.pug             -> __tmpl/index.html                   -> __tmpl/index.html            ',
+			'__tmpl/000_home.pug          -> __tmpl/000_home/index.html          -> __tmpl/000_home.html         ',
+			'__tmpl/100_sub.pug           -> __tmpl/100_sub/index.html           -> __tmpl/100_sub.html          ',
+			'__tmpl/200_blog_index.pug    -> __tmpl/200_blog_index/index.html    -> __tmpl/200_blog_index.html   ',
+			'__tmpl/210_blog_index.pug    -> __tmpl/210_blog_index/index.html    -> __tmpl/210_blog_index.html   ',
+			'__tmpl/300_form_input.pug    -> __tmpl/300_form_input/index.html    -> __tmpl/300_form_input.html   ',
+			'__tmpl/301_form_confirm.pug  -> __tmpl/301_form_confirm/index.html  -> __tmpl/301_form_confirm.html ',
+			'__tmpl/302_form_complete.pug -> __tmpl/302_form_complete/index.html -> __tmpl/302_form_complete.html',
+			'sub-folder.test/index.pug    -> sub-folder.test/index.html          -> sub-folder.test/index.html   ',
 		]);
 	});
 });
