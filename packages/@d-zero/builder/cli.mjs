305b31482f2255ef667d8a6e9015cab5522452f8
YusukeHirao <hirao@d-zero.co.jp>
2024-10-11 08:30:30 +0900

    refactor(builder): remove watch and serve process

--- packages/@d-zero/builder/cli.mjs
+++ packages/@d-zero/builder/cli.mjs
@@ -1,58 +1,16 @@
 #!/usr/bin/env node
 
-import { rmSync } from 'node:fs';
-
 import Eleventy from '@11ty/eleventy';
 
 import { build } from './fn/build.mjs';
 
-/**
- * Serve and watch mode
- */
-const isServe = process.env.NODE_ENV === 'serve';
-
-cleanUp(true);
-
 const elev = new Eleventy(
 	undefined, // inputDir is set from the Eleventy config file
 	undefined, // outputDir is set from the Eleventy config file
-	isServe
-		? {
-				runMode: 'serve',
-				incremental: true,
-			}
-		: {
-				quietMode: true,
-			},
+	{
+		quietMode: true,
+	},
 );
 
-if (!isServe) {
-	// Generate the site
-	await build(elev);
-	process.exit(0);
-}
-
-// Serve the site in watch mode
-await elev.watch();
-elev.serve();
-
-// Clean up after the process is aborted
-process.stdin.resume();
-process.on('SIGINT', cleanUp);
-process.on('SIGTERM', cleanUp);
-process.on('exit', cleanUp);
-
-/**
- * Clean up
- *
- * Remove the `.serve` and `.11ty-vite` directories
- * Before starting the build process.
- * Also, After aborting the process.
- */
-function cleanUp(silent = false) {
-	if (!silent) {
-		process.stdout.write('🧹 Clean up\n');
-	}
-	rmSync('.serve', { recursive: true, force: true });
-	rmSync('.11ty-vite', { recursive: true, force: true });
-}
+// Generate the site
+await build(elev);
