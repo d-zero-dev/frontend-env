6822290857720a0345fb02204ce1b7310b248a72
YusukeHirao <hirao@d-zero.co.jp>
2024-10-11 17:00:01 +0900

    refactor(scaffold): update build options

--- packages/@d-zero/scaffold/eleventy.config.mjs
+++ packages/@d-zero/scaffold/eleventy.config.mjs
@@ -3,21 +3,66 @@ import path from 'node:path';
 import eleventy from '@d-zero/builder/11ty';
 
 export default function (eleventyConfig) {
-	eleventyConfig.addGlobalData('alias', {
-		'@': path.resolve(import.meta.dirname, '__assets', '_libs'),
+	return eleventy(eleventyConfig, {
+		/**
+		 * Alias for the path to the directory containing the components.
+		 */
+		alias: {
+			'@': path.resolve(import.meta.dirname, '__assets', '_libs'),
+		},
+
+		/**
+		 * Output directory for CSS files.
+		 */
+		// outputCssDir: 'css',
+
+		/**
+		 * Output directory for JavaScript files
+		 */
+		// outputJsDir: 'js',
+
+		/**
+		 * Output directory for image files
+		 */
+		// outputImgDir: 'img',
+
+		/**
+		 * Prettier options.
+		 *
+		 * @see https://prettier.io/docs/en/options
+		 */
+		// prettier: false,
+
+		/**
+		 * Minifier options.
+		 *
+		 * @see https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference
+		 */
+		// minifier: { minifyJS: false },
+
+		/**
+		 * Line break.
+		 */
+		// lineBreak: '\r\n',
+
+		/**
+		 * Character encoding.
+		 */
+		// charset: 'shift_jis',
+
+		/**
+		 * Path format
+		 */
+		// pathFormat: 'directory',
+
+		/**
+		 * Automatically decode the content on the dev server.
+		 */
+		// autoDecode: true,
+
+		/**
+		 * Server Side Include options on the dev server.
+		 */
+		// ssi: { '**/*': { encoding: 'shift_jis' } },
 	});
-
-	// eleventyConfig.addGlobalData('outputCssDir', 'css');
-	// eleventyConfig.addGlobalData('outputJsDir', 'js');
-	// eleventyConfig.addGlobalData('outputImgDir', 'img');
-	// eleventyConfig.addGlobalData('prettier', false);
-	/**
-	 * @see https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference
-	 */
-	// eleventyConfig.addGlobalData('minifier', { minifyJS: false });
-	// eleventyConfig.addGlobalData('lineBreak', '\r\n');
-	// eleventyConfig.addGlobalData('charset', 'shift_jis');
-	// eleventyConfig.addGlobalData('pathFormat', 'directory');
-
-	return eleventy(eleventyConfig);
 }
