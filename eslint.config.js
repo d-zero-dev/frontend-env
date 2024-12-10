f7f3ecf1aaa5337720cd31cbe88c6b9fd2c0b5b8
YusukeHirao <hirao@d-zero.co.jp>
2024-12-10 12:15:41 +0900

    chore(repo): update eslint config to flat

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import path from 'node:path';
+
+import dz from '@d-zero/eslint-config';
+
+import scaffold from './packages/@d-zero/scaffold/eslint.config.js';
+
+/**
+ * @type {import('eslint').Linter.Config[]}
+ */
+export default [
+	...dz.configs.node,
+	...scaffold,
+	{
+		files: ['**/{*.{config,spec}.{js,mjs,ts},*.*rc,*.*rc.{js,mjs}}'],
+		rules: {
+			'import/no-extraneous-dependencies': 0,
+		},
+	},
+	{
+		ignores: [
+			...scaffold.flatMap(
+				(config) =>
+					config.ignores?.map((ignorePath) =>
+						path.join('packages', '@d-zero', 'scaffold', ignorePath),
+					) ?? [],
+			),
+			'**/dist/**/*',
+		],
+	},
+];
