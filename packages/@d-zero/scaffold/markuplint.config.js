e302169a04d5e45afe0b5f6b3b080846e9fe496b
YusukeHirao <hirao@d-zero.co.jp>
2025-01-06 15:53:33 +0900

    fix(scaffold): fix markuplint config

--- packages/@d-zero/scaffold/markuplint.config.js
+++ packages/@d-zero/scaffold/markuplint.config.js
@@ -1,10 +1,22 @@
 import { extendsConfig } from '@d-zero/markuplint-config';
 
+const extended = extendsConfig({
+	// classNaming: ['/^splide(?:__[a-z]+)?$/'],
+});
+
 export default {
-	...extendsConfig({
-		// classNaming: ['/^splide(?:__[a-z]+)?$/'],
-	}),
+	...extended,
 	nodeRules: [
+		...extended.nodeRules,
+		{
+			// Revert requiring `width` and `height` attributes from the preset config.
+			// @see https://github.com/markuplint/markuplint/blob/dev/packages/%40markuplint/config-presets/src/preset.performance.json
+			// Due to assigning them automatically by the build process.
+			selector: 'img[src]',
+			rules: {
+				'required-attr': false,
+			},
+		},
 		{
 			selector: '.c-pagination a',
 			rules: {
