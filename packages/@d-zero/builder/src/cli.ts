a09aa0b304a7c693a2b856314a39ffa25f35141e
Yusuke Hirao <hirao@CPU-M0089.local>
2025-10-03 21:30:32 +0900

    feat(builder): add --clear-cache option to CLI

--- packages/@d-zero/builder/src/cli.ts
+++ packages/@d-zero/builder/src/cli.ts
@@ -2,11 +2,18 @@
 import Eleventy from '@11ty/eleventy';
 
 import { build } from './build.js';
+import { clearAllContentCache } from './content-cache.js';
 
 /**
  *
  */
 export async function cli() {
+	const clearOptions = process.argv.includes('--clear-cache');
+
+	if (clearOptions) {
+		await clearAllContentCache();
+	}
+
 	const elev = new Eleventy(
 		undefined, // inputDir is set from the Eleventy config file
 		undefined, // outputDir is set from the Eleventy config file
